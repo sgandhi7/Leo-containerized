f56102bb5a6f60c621a48cbe5ed2b60a0f5fdad5
kildre <kildre@gmail.com>
2023-12-04 12:42:03 -0500

    ui-history-page_updates # updated tests, still needs work but will come back to it

--- src/pages/history/history.test.tsx
+++ src/pages/history/history.test.tsx
@@ -1,8 +1,27 @@
-import { act, render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { act, render, screen } from '@testing-library/react';
 import { AuthProvider } from 'react-oidc-context';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import { History } from './history';
+// Mock the useApi hook
+jest.mock('@src/hooks/use-api', () => ({
+  useApi: () => ({
+    getItems: jest.fn(),
+    items: {
+      items: [
+        {
+          id: 1,
+          name: 'Test Investigation',
+          created: new Date().toISOString(),
+          createdBy: 'John Doe',
+          status: 'Active',
+          prompts: [],
+        },
+      ],
+    },
+  }),
+}));
 
 describe('History', () => {
   const componentWrapper = (
@@ -20,5 +39,19 @@ describe('History', () => {
     await act(async () => {
       expect(baseElement).toBeTruthy();
     });
+  });
+
+  test('displays investigation data in the table', () => {
+    render(<History />);
+
+    test('displays the Share button', () => {
+      render(<History />);
+      expect(screen.getByText('Share')).toBeInTheDocument();
+    });
+  });
+
+  test('displays the Share button', () => {
+    render(<History />);
+    expect(screen.getByText('Share')).toBeInTheDocument();
   });
 });
