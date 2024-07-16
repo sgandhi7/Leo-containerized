c6b51880f3ffa86672635fb00615889dbf91bcd2
neharavn <neharavin1@gmail.com>
2024-07-16 14:33:25 -0500

    Fixed sign in test issues

--- src/pages/sign-in/sign-in.test.tsx
+++ src/pages/sign-in/sign-in.test.tsx
@@ -1,11 +1,7 @@
-/*
-import { act, render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { act, render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 
 import { RecoilRoot } from 'recoil';
-import * as useAuthMock from '../../hooks/use-auth';
-import { User } from '../../types/user';
 import { SignIn } from './sign-in';
 
 describe('SignIn', () => {
@@ -17,8 +13,8 @@ describe('SignIn', () => {
     </RecoilRoot>
   );
 
-  const mockUsername = 'username1';
-  const mockPassword = 'test1234';
+  //const mockUsername = 'username1';
+  //const mockPassword = 'test1234';
 
   const OLD_ENV = process.env;
   beforeEach(() => {
@@ -31,7 +27,7 @@ describe('SignIn', () => {
       expect(baseElement).toBeTruthy();
     });
   });
-
+  /*
   /test('should simulate a login attempt with blank fields', async () => {
     const { baseElement } = render(signInComponent);
     await userEvent.click(
@@ -143,5 +139,5 @@ describe('SignIn', () => {
     );
     expect(baseElement.querySelectorAll('.usa-error-message').length).toBe(0);
   });
+  */
 });
-*/
